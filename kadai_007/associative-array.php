3faa9b117d8ba27b387ee89d32f9242859b3ced4
Akikokuri <akikokurikuri@gmail.com>
2024-10-02 14:13:16 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>課題連想配列を作って値とキーを出力しよう</title>
+</head>
+<body>
+
+    <?php
+    $product_one = ['name'=>'onion', 'price'=> 200, 'weight'=>'160'];
+
+    print_r($product_one);
+
+    ?>
+    
+</body>
+</html>
